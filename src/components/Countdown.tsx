9681b849a2f2720f866c7c9fa8352304085b8c8e
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-02-25 01:27:41 -0300

    Adjusting the keyboard tab size...

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,46 +2,46 @@ import { useState, useEffect } from 'react'
 import styles from '../styles/components/countdown.module.css'
 
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60)
-    const [active, setActive] = useState(false)
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
 
-    const minutes = Math.floor(time / 60)
-    const seconds = time % 60
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
 
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
-    function startCountdown() {
-        setActive(true)
+  function startCountdown() {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if (active === true && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
     }
+  }, [active, time])
 
-    useEffect(() => {
-        if (active === true && time > 0) {
-            setTimeout(() => {
-                setTime(time - 1)
-            }, 100)
-        }
-    }, [active, time])
-
-    return (
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
         <div>
-            <div className={styles.countdownContainer}>
-                <div>
-                    <span>{minuteLeft}</span>
-                    <span>{minuteRight}</span>
-                </div>
-                <span>:</span>
-                <div>
-                    <span>{secondLeft}</span>
-                    <span>{secondRight}</span>
-                </div>
-            </div>
-            <button type="button"
-                className={styles.countdownButton}
-                onClick={startCountdown}
-            >
-                Iniciar um ciclo
-            </button>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
-    )
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+      <button type="button"
+        className={styles.countdownButton}
+        onClick={startCountdown}
+      >
+        Iniciar um ciclo
+            </button>
+    </div>
+  )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,16 @@
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/crummenauerca.png" alt="Imagem de Cezar Augusto Crummenauer" />
-            <div>
-                <strong>Cezar Augusto Crummenauer</strong>
-                <p>
-                    <img src="icons/level.svg" alt="Ícone de nível" />
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/crummenauerca.png" alt="Imagem de Cezar Augusto Crummenauer" />
+      <div>
+        <strong>Cezar Augusto Crummenauer</strong>
+        <p>
+          <img src="icons/level.svg" alt="Ícone de nível" />
                     Level 1
                 </p>
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }

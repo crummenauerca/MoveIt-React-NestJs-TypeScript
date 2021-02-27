f6efc481b840c86cceff485e9108f4394c68cdd8
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2021-02-27 20:21:48 -0300

    Getting user progress using cookies on getServerSideProps function from NextJs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,53 @@ import styles from '../styles/pages/Home.module.css'
 import { Countdown } from "../components/Countdown"
 import { Profile } from "../components/Profile"
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { CountdownProvider } from "../contexts/CountdownContext"
+import { ChallengesProvider } from "../contexts/ChallengesContext"
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompleteChallanges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.currentExperience}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallanges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
